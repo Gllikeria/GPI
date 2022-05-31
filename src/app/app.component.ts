6fa9eb9b99627c110ca7ad6a5a2646f48ebfdaed
Gllikeria <l_ghunashvili@cu.edu.ge>
2022-05-31 11:53:40 +0400

    final markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'gpi';
+  testArray = [true, false, false];
+  testFunction(position: number) {
+    this.testArray[0] = false;
+    this.testArray[1] = false;
+    this.testArray[2] = false;
+
+    this.testArray[position] = true;
+
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'Mtavruli': ['Mtavruli', 'system-ui'],
+    }
+  },
+ };
